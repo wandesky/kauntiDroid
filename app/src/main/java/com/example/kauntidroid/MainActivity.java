d37396f052a2dd34c28ee0edd5a100b9da12ac0f
wandesky <bwandesky@gmail.com>
2019-03-15 00:21:19 +0300

    failed attempt to process json using volley

--- app/src/main/java/com/example/kauntidroid/MainActivity.java
+++ app/src/main/java/com/example/kauntidroid/MainActivity.java
@@ -13,6 +13,19 @@ import com.google.zxing.Result;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
+
+//Extra imports for http requests
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
@@ -62,16 +75,66 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         scannerView.stopCamera();
     }
+    String resultCode;
+    String resultReport = "NO RESULT RECEIVED";
 
     class ZXingScannerResultHandler implements ZXingScannerView.ResultHandler{
         @Override
         public void handleResult(Result result){
-            String resultCode = result.getText();
+            resultCode = result.getText();
             Toast.makeText(MainActivity.this, resultCode, Toast.LENGTH_SHORT).show();
+
+            //Start of http request process
+            try {
+                sendRequest();
+            } catch (IOException e) {
+                Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            } catch (JSONException e) {
+                Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+
+            Toast.makeText(MainActivity.this, resultReport, Toast.LENGTH_SHORT).show();
 
             setContentView(R.layout.activity_main);
             scannerView.stopCamera();
+
         }
     }
+
+
+    //methods to retrieve data from server
+    private String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
+    }
+
+
+    public void sendRequest() throws IOException, JSONException {
+        JSONObject json = readJsonFromUrl("https://kaunti.herokuapp.com/api/v1/permits/TCG-SBP-2018-2019-1276");
+        resultReport = json.toString();
+//        System.out.println(json.toString());
+//        System.out.println(json.get("permitID"));
+
+//        Toast.makeText(MainActivity.this, json.toString(), Toast.LENGTH_LONG).show();
+    }
+
 
 }
