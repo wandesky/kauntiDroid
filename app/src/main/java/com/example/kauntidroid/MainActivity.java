1867c78de16e2f45c60f339fc8cce2454266c9a2
wandesky <bwandesky@gmail.com>
2019-03-14 21:15:20 +0300

    set QR code scanner --working

--- app/src/main/java/com/example/kauntidroid/MainActivity.java
+++ app/src/main/java/com/example/kauntidroid/MainActivity.java
@@ -5,7 +5,13 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,16 +26,18 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+//                case R.id.navigation_dashboard:
+//                    mTextMessage.setText(R.string.title_dashboard);
+//                    return true;
+//                case R.id.navigation_notifications:
+//                    mTextMessage.setText(R.string.title_notifications);
+//                    return true;
             }
             return false;
         }
     };
+
+    private ZXingScannerView scannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +47,31 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    public void scanCode(View view){
+        scannerView = new ZXingScannerView(this);
+        scannerView.setResultHandler(new ZXingScannerResultHandler());
+
+        setContentView(scannerView);
+        scannerView.startCamera();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        scannerView.stopCamera();
+    }
+
+    class ZXingScannerResultHandler implements ZXingScannerView.ResultHandler{
+        @Override
+        public void handleResult(Result result){
+            String resultCode = result.getText();
+            Toast.makeText(MainActivity.this, resultCode, Toast.LENGTH_SHORT).show();
+
+            setContentView(R.layout.activity_main);
+            scannerView.stopCamera();
+        }
     }
 
 }
