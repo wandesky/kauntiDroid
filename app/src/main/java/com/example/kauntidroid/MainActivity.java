0609e26b1baf58bd0502b6d7bb3ff0e0016b6c50
wandesky <bwandesky@gmail.com>
2019-03-15 08:36:41 +0300

    sketchy but presentable MVP

--- app/src/main/java/com/example/kauntidroid/MainActivity.java
+++ app/src/main/java/com/example/kauntidroid/MainActivity.java
@@ -27,6 +27,14 @@ import java.nio.charset.Charset;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+//for the sake of intents
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
@@ -40,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
-                    reportData.setText(resultReport);
+//                    reportData.setText(resultReport);
                     return true;
 //                case R.id.navigation_dashboard:
 //                    mTextMessage.setText(R.string.title_dashboard);
@@ -60,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        reportData = (TextView)findViewById(R.id.permitsTextView);
+        reportData = (TextView)findViewById(R.id.permitsTextView);
 //        reportData.setText(resultReport);
 
         mTextMessage = (TextView) findViewById(R.id.message);
@@ -83,12 +91,13 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         scannerView.stopCamera();
     }
-    String resultCode;
+    String resultCode, response, businessType, validTill, businessPremises, plotNumber;
     String resultReport = "NO RESULT RECEIVED";
+    String customerName;
 
-    public class Connection extends AsyncTask<Void, Void, String>{
+    public class Connection extends AsyncTask<Void, Void, Void>{
         @Override
-        protected String doInBackground(Void... args){
+        protected Void doInBackground(Void... args){
             //Start of http request process
             try {
                 sendRequest();
@@ -101,18 +110,42 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 //            reportData.setText(resultReport);
-            return "OK";
+            return null;
         }
 
         @Override
-        protected void onPostExecute(String result){
+        protected void onPostExecute(Void result){
             super.onPostExecute(result);
 //            reportData.setText(resultReport);
 
-            reportData = (TextView)findViewById(R.id.permitsTextView);
-            reportData.setText(resultReport);
+//            reportData = (TextView)findViewById(R.id.permitsTextView);
+//            reportData.setText(resultReport);
+            System.out.println("*****ON POST EXECUTE CALLED****** ");
+            generateReport();
         }
     }
+
+    public void generateReport() {
+        Intent intent = new Intent(MainActivity.this, ReportingActivity.class);
+        intent.putExtra("response", response);
+        intent.putExtra("id", resultReport);
+        intent.putExtra("customer", customerName);
+        intent.putExtra("businessType", businessType);
+        intent.putExtra("validTill", validTill);
+        intent.putExtra("businessPremises", businessPremises);
+        intent.putExtra("plotNumber", plotNumber);
+
+        startActivity(intent);
+
+
+//        Intent intent = new Intent(this, ReportingActivity.class);
+////        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = resultReport;
+//        intent.putExtra("ID", message);
+//        intent.putExtra
+//        startActivity(intent);
+    }
+
 
     class ZXingScannerResultHandler implements ZXingScannerView.ResultHandler{
         @Override
@@ -121,6 +154,9 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this, resultCode, Toast.LENGTH_SHORT).show();
 
             setContentView(R.layout.activity_main);
+            reportData = (TextView)findViewById(R.id.permitsTextView);
+            reportData.setText("Please wait for the results to load");
+
             scannerView.stopCamera();
 
             new Connection().execute();
@@ -157,13 +193,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public String sendRequest() throws IOException, JSONException {
-        reportData.setText("SEND REQUEST METHOD CALLED");
-        JSONObject json = readJsonFromUrl("https://kaunti.herokuapp.com/api/v1/permits/TCG-SBP-2018-2019-1276");
-        resultReport = "RESULTS \n";
+    public void sendRequest() throws IOException, JSONException {
+//        resultCode = "TCG-SBP-2018-2019-1276";
+//        reportData.setText("SEND REQUEST METHOD CALLED");
+        String url = "https://kaunti.herokuapp.com/api/v1/permits/" + resultCode ;
+        JSONObject json = readJsonFromUrl(url);
+
+
+//        resultReport = "RESULTS \n";
         resultReport = json.get("permitID").toString();
         System.out.println("*****RESULT REPORT ****** " + resultReport);
-        return "OK";
+
+        //Checking if results are valid
+        if(resultReport.contains("invalid")){
+            response = "FAKE";
+//            return;
+        }
+        else{
+            response = "GENUINE";
+            customerName = json.get("customer").toString();
+            businessType = json.get("businessType").toString();
+            validTill = json.get("validTill").toString();
+            businessPremises = json.get("businessAddress").toString();
+            plotNumber = json.get("plotNumber").toString();
+        }
+
+//        reportData.setText(resultReport);
+//        return null;
 //        reportData.setText(resultReport);
 
 
--- app/src/main/java/com/example/kauntidroid/ReportingActivity.java
+++ app/src/main/java/com/example/kauntidroid/ReportingActivity.java
@@ -1,13 +1,59 @@
 package com.example.kauntidroid;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ReportingActivity extends AppCompatActivity {
+
+    TextView results, tv1, customer, businessType, validTill, businessPremises, plotNumber, info;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporting);
+
+        results = (TextView) findViewById(R.id.results_view);
+        tv1 = (TextView) findViewById(R.id.textView2);
+        customer = (TextView) findViewById(R.id.customer_view);
+        businessType = (TextView) findViewById(R.id.businessType_view);
+        validTill = (TextView) findViewById(R.id.expiry_view);
+        businessPremises = (TextView) findViewById(R.id.premises_view);
+        plotNumber = (TextView) findViewById(R.id.plotNumber_view);
+        info = (TextView) findViewById(R.id.info_view);
+
+
+        if(getIntent().getStringExtra("response").contains("FAKE")){
+            results.setTextColor(Color.RED);
+            results.setText("FAKE PERMIT");
+            tv1.setText("INVALID");
+            customer.setText("INVALID");
+            businessType.setText("INVALID");
+            validTill.setText("INVALID");
+            businessPremises.setText("INVALID");
+            plotNumber.setText("INVALID");
+            info.setText("WARNING: "
+            + "This permit does not exist in the county records ");
+        }
+        else{
+            results.setTextColor(Color.GREEN);
+            results.setText("GENUINE PERMIT");
+            tv1.setText(getIntent().getStringExtra("id"));
+            customer.setText(getIntent().getStringExtra("customer"));
+            businessType.setText(getIntent().getStringExtra("businessType"));
+            validTill.setText(getIntent().getStringExtra("validTill"));
+            businessPremises.setText(getIntent().getStringExtra("businessPremises"));
+            plotNumber.setText(getIntent().getStringExtra("plotNumber"));
+            info.setText("MESSAGE: "
+                    + "A copy of this permit was found in the county records "
+            + "If the details below match that of the printed permit "
+            + "Then the scanned permit is genuine");
+
+        }
+
     }
 }
